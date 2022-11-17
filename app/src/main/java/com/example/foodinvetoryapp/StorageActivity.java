61e7569969c68dda8e3b578298a321363f5835a9
Radek707 <rnowosielski77@gmail.com>
2022-11-17 02:36:49 +0100

    Storage activity setup, saving and deleting Storage tested.

--- app/src/main/java/com/example/foodinvetoryapp/StorageActivity.java
+++ app/src/main/java/com/example/foodinvetoryapp/StorageActivity.java
@@ -2,13 +2,77 @@ package com.example.foodinvetoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.foodinvetoryapp.models.Storage;
+import com.example.foodinvetoryapp.repository.MyRepository;
+import com.example.foodinvetoryapp.repository.RepositoryProvider;
 
 public class StorageActivity extends AppCompatActivity {
+
+    private static final int NO_STORAGE = -1;
+    private MyRepository myRepository;
+    private Storage storage;
+    private long storageId;
+    private EditText addStorageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_storage);
+        setContentView(R.layout.activity_storage);
+
+        myRepository = RepositoryProvider.getInstance(this);
+
+        Button saveButton = findViewById(R.id.saveButton);
+        Button deleteButton = findViewById(R.id.deleteButton);
+
+        saveButton.setOnClickListener(view -> saveStorage());
+
+        addStorageEditText = findViewById(R.id.addStorageEditText);
+
+        Intent intent = getIntent();
+        storageId = intent.getLongExtra(TAG.STORAGE_ID, NO_STORAGE);
+
+        if (storageId != NO_STORAGE) {
+            deleteButton.setOnClickListener(view -> deleteStorage());
+            deleteButton.setVisibility(View.VISIBLE);
+            storage = myRepository.getStorageById(storageId);
+            updateUI(storage);
+        } else {
+            deleteButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void updateUI(Storage storage) {
+        if (storage != null) {
+            addStorageEditText.setText(storage.getStorageName());
+        }
+    }
+
+    private void deleteStorage() {
+        myRepository.deleteStorage(storageId);
+        Toast.makeText(this, "Storage deleted", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void saveStorage() {
+        String storageName = addStorageEditText.getText().toString();
+
+        if (storage == null) {
+            Storage storage = new Storage();
+            storage.setStorageName(storageName);
+            myRepository.addStorage(storage);
+        } else {
+            storage.setStorageName(storageName);
+            myRepository.editStorage(storage);
+        }
+
+        Toast.makeText(this, "Storage saved.", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
